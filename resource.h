a08331d7c20c1fe0394cc2fa92c41975ab7753db
Baltazarus <93092559+Baltazarvs@users.noreply.github.com>
2022-02-28 16:35:47 +0100

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,12 +1,12 @@
-#define IDD_INSERT_ITEM                 101
-#define IDR_MENUBAR                     103
+#define IDD_INSERT_ITEM         101
+#define IDR_MENUBAR             103
 
-#define IDC_EDIT_ITEMTEXT               1001
-#define IDC_STATIC_ABOUT				        1101
+#define IDC_EDIT_ITEMTEXT       1001
+#define IDC_STATIC_ABOUT        1101
 
-#define ID_ITEM_INSERT                  40001
-#define ID_ITEM_CLEARLIST               40002
-#define ID_ITEM_EXIT                    40003
-#define ID_ITEM_LOAD_FROM_FILE          40004
-#define ID_HELP_ABOUT					          40100
-#define IDD_ABOUT						            41000
+#define ID_ITEM_INSERT          40001
+#define ID_ITEM_CLEARLIST       40002
+#define ID_ITEM_EXIT            40003
+#define ID_ITEM_LOAD_FROM_FILE  40004
+#define ID_HELP_ABOUT					  40100
+#define IDD_ABOUT						    41000
